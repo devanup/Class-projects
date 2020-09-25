be7eb3e32e776cd86be51ce196f0156fdaeb9c57
Anup <66684609+devanup@users.noreply.github.com>
2020-09-25 17:26:52 -0400

    added week-1 files
    added week-1 assignments with 2 files

---
+++ week1/lab1_1st_program.py
@@ -0,0 +1,6 @@
+#Name: Anup Thapa
+#Email:  anup.thapa65@myhunter.cuny.edu
+#Date: August 27, 2018
+#This program prints: Hello, World!
+
+print("Hello, World!")
---
+++ week1/lab1_2nd_program.py
@@ -0,0 +1,15 @@
+#Name:  Anup Thapa
+#Email: anup.thapa65@myhunter.cuny.edu
+#Date: September 2, 2020
+#This program draws a star.
+
+import turtle
+
+wn = turtle.Screen()
+thomasH = turtle.Turtle()
+
+for i in range(5):
+     thomasH.forward(150)
+     thomasH.left(144)
+
+wn.exitonclick()
